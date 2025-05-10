a874ddbaa48422e42e84c20598688f843c6b2913
pvini07BR <pvini.almeida.lima@gmail.com>
2025-05-10 16:18:00 -0300

    new stuff

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,5 +1,8 @@
+vim.cmd("set nowrap")
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.opt.statuscolumn = "%@SignCb@%s%=%T%@NumCb@%l│%T"
 vim.g.mapleader = " "
