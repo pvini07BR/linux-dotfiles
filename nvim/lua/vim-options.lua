2394225b11b0a91f18500f840930883c95501fbc
pvini07BR <pvini07br@proton.me>
2025-05-29 18:43:23 -0300

    now it uses stow format

--- nvim/lua/vim-options.lua
+++
@@ -1,8 +0,0 @@
-vim.cmd("set nowrap")
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set number")
-vim.cmd("set relativenumber")
-vim.opt.statuscolumn = "%@SignCb@%s%=%T%@NumCb@%l│%T"
-vim.g.mapleader = " "
